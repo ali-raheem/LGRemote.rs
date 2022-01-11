dc61775919fab74695a6b3a500248e73d52cae36
Ali Raheem <ali-windows@shoryuken.me>
2022-01-11 19:52:03 +0000

    Clean up error handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use reqwest::{Client, Response};
 use std::collections::HashMap;
 use std::env;
+use std::process::exit;
 
 #[tokio::main]
 async fn main() {
@@ -78,20 +79,30 @@ async fn main() {
 	println!("Usage:\n\t{} IP [KEY] [COMMAND]\n\nThe IP address must be provided.
 If only an IP is provided the TV will be asked to display it's pairing code.
 Subsequent requests will try and pair and runany command requested.", args[0]);
+	   exit(1);
     }
     let ip = args[1].clone();
     let client = reqwest::Client::new();
 
     if args.len() < 4 {
 	let res = display_pair_key(&ip, client.clone());
-	println!("Response status {}", res.await.status());
+	if 200 == res.await.status() {
+	   println!("Requested authkey to be displayed.");
+	   exit(0);
+	} else {
+	  println!("Failed to request auth key. Check IP.");
+	  exit(1);
+	}
     }
 
-    let key: u32 = args[2].parse().unwrap();
+    let key: u32 = args[2].parse().expect("Check Key it should be a number.");
     let command = COMMAND_CODES[&args[3] as &str];
         
     let res = pair_with_key(&ip, client.clone(), key);
-    println!("Response status {}", res.await.status());
+    if 200 != res.await.status() {
+       println!("Auth failed check IP/Key..");
+       exit(2);
+    }
 
     let res = send_command(&ip, client.clone(), command);
     println!("Response status {}", res.await.status());
@@ -102,8 +113,7 @@ async fn display_pair_key(ip: &str, client: Client) -> Response {
         <?xml version=\"1.0\" encoding=\"utf-8\"?>
         <auth>
             <type>AuthKeyReq</type>
-        </auth>
-   ";
+        </auth>";
     let url = format!("http://{}:8080/roap/api/auth", ip);
     client
         .post(url)
@@ -111,7 +121,7 @@ async fn display_pair_key(ip: &str, client: Client) -> Response {
         .body(display_pair_key)
         .send()
         .await
-        .expect("send")
+        .expect("Failed to display key check IP.")
 }
 
 async fn pair_with_key(ip: &str, client: Client, key: u32) -> Response {
@@ -132,7 +142,7 @@ async fn pair_with_key(ip: &str, client: Client, key: u32) -> Response {
         .body(auth_request)
         .send()
         .await
-        .expect("send")
+        .expect("Failed to pair. Check IP.")
 }
 
 async fn send_command(ip: &str, client: Client, cmd_code: u32) -> Response {
@@ -154,7 +164,6 @@ async fn send_command(ip: &str, client: Client, cmd_code: u32) -> Response {
         .body(send_command)
         .send()
         .await
-        .expect("send")
+        .expect("Failed to send command, check IP, Key and Command availability.")
 }
 
-//fn main() {}
