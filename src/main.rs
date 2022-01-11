f0b18db89c72a323245e711bac389ec58a36f1fc
Ali Raheem <ali-windows@shoryuken.me>
2022-01-11 22:39:36 +0000

    Using TOML file for commands

--- src/main.rs
+++ src/main.rs
@@ -2,78 +2,14 @@ use reqwest::{Client, Response};
 use std::collections::HashMap;
 use std::env;
 use std::process::exit;
+use toml::Value;
+use std::fs;
 
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let COMMAND_CODES: HashMap<&str, u32> = HashMap::from([
-        ("POWER", 1),
-        ("NUM_0", 2),
-        ("NUM_1", 3),
-        ("NUM_2", 4),
-        ("NUM_3", 5),
-        ("NUM_4", 6),
-        ("NUM_5", 7),
-        ("NUM_6", 8),
-        ("NUM_7", 9),
-        ("NUM_8", 10),
-        ("NUM_9", 11),
-        ("UP", 12),
-        ("DOWN", 13),
-        ("LEFT", 14),
-        ("RIGHT", 15),
-        ("OK", 20),
-        ("HOME", 21),
-        ("MENU", 22),
-        ("BACK", 23),
-        ("VOLUME_UP", 24),
-        ("VOLUME_DOWN", 25),
-        ("MUTE", 26),
-        ("CHANNEL_UP", 27),
-        ("CHANNEL_DOWN", 28),
-        ("BLUE", 29),
-        ("GREEN", 30),
-        ("RED", 31),
-        ("YELLOW", 32),
-        ("PLAY", 33),
-        ("PAUSE", 34),
-        ("STOP", 35),
-        ("FF", 36),
-        ("REW", 37),
-        ("SKIP_FF", 38),
-        ("SKIP_REW", 39),
-        ("REC", 40),
-        ("REC_LIST", 41),
-        ("LIVE", 43),
-        ("EPG", 44),
-        ("INFO", 45),
-        ("ASPECT", 46),
-        ("EXT", 47),
-        ("PIP", 48),
-        ("SUBTITLE", 49),
-        ("PROGRAM_LIST", 50),
-        ("TEXT", 51),
-        ("MARK", 52),
-        ("3D", 400),
-        ("3D_LR", 401),
-        ("DASH", 402),
-        ("PREV", 403),
-        ("FAV", 404),
-        ("QUICK_MENU", 405),
-        ("TEXT_OPTION", 406),
-        ("AUDIO_DESC", 407),
-        ("NETCAST", 408),
-        ("ENERGY_SAVE", 409),
-        ("AV", 410),
-        ("SIMPLINK", 411),
-        ("EXIT", 412),
-        ("RESERVED", 413),
-        ("PIP_CHANNEL_UP", 414),
-        ("PIP_CHANNEL_DOWN", 415),
-        ("PIP_SWITCH", 416),
-        ("APPS", 417),
-    ]);
+    let commands = &fs::read_to_string("/etc/LGremote/commands.toml").unwrap().parse::<Value>().unwrap();
 
     if args.len() < 2 {
 	println!("Usage:\n\t{} IP [KEY] [COMMAND]\n\nThe IP address must be provided.
@@ -96,13 +32,7 @@ Subsequent requests will try and pair and runany command requested.", args[0]);
     }
 
     let key: u32 = args[2].parse().expect("Check Key it should be a number.");
-    let command = match COMMAND_CODES.get(&args[3] as &str) {
-	Some(c) => c.clone(),
-	None => {
-	    println!("Command not found.");
-	    exit(1)
-	}
-    };
+    let command = commands[&args[3] as &str].as_integer().unwrap() as u32;
         
     let res = pair_with_key(&ip, client.clone(), key);
     if 200 != res.await.status() {
